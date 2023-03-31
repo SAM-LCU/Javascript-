953909f34869219c71e32ae5570f8ae61c9dab2e
SAM-LCU <streetstory2000@gmail.com>
2023-03-31 00:42:58 -0700

    basics

---
+++ jsbasic/one.js
@@ -0,0 +1,15 @@
+var x = 100;
+var y = 780;
+console.log(x+y)
+console.log("hello gm")
+
+
+// single line comments
+
+/*
+multi line comments 
+-comments are non excuteable statements
+*/
+var a = 100;
+var b = "Hello";
+console.log(a+b)
