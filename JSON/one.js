ee96ed05e230a00bd2de42318e4df950bbebc96e
SAM-LCU <streetstory2000@gmail.com>
2023-04-07 01:47:30 -0700

    jason_basics

---
+++ JSON/one.js
@@ -0,0 +1,15 @@
+// WAP to print thedata using for off and for loop
+let emp = [{id:101, name:"sam", sal:45000, loc:"Bangalore"},
+{id:101, name:"sam", sal:45000, loc:"Bangalore"},
+{id:101, name:"sam", sal:45000, loc:"Bangalore"}]
+// print employess names
+
+/*for(emp of emp){
+    console.log(emp);   // using for (of) loop 
+}*/
+ 
+
+/*for(var i=0; i<=emp.length-1; i=i+1){
+    console.log(emp[i].id);               // using for loop for()
+}*/
+
