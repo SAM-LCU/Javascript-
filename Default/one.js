f7d5aa07aa2443b1a517a53c34fa1855245c1da8
SAM-LCU <streetstory2000@gmail.com>
2023-04-29 06:19:25 -0700

    basic

---
+++ Default/one.js
@@ -0,0 +1,7 @@
+let hero_name = "kamal hassan";
+let get_Name = ( ) => {
+    console.log("The God Of Indian Cinema");
+}
+
+module.exports = hero_name; // Es5 Version Code
+// export default hero_name    // Es6 Version Code
---
+++ Default/two.js
@@ -0,0 +1,3 @@
+// import hero_Name from './one' // Es6 Version code
+const hero_name = require('./one')
+console.log(hero_name);
