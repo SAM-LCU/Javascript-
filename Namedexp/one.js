13e7f54ee09f0cca553e124a34718a80cfc8403e
SAM-LCU <streetstory2000@gmail.com>
2023-05-01 05:13:47 -0700

    named_exports

---
+++ Namedexp/one.js
@@ -0,0 +1,7 @@
+let hero_name = "Kamal Hassan"
+let get_name = ( ) => {
+    console.log("God Of Indian");
+}
+// Es6 version code {hero_name,get_name} it will not work here
+// Es5 version code 
+module.exports = {hero_name,get_name}
---
+++ Namedexp/two.js
@@ -0,0 +1,4 @@
+// import { hero_name,get_name } from "./one"; Es6 version of code the will not excute
+// Es5 version Code
+const {hero_name,get_name} = require('./one')
+get_name();
